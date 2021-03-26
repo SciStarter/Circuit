42864a8fd18b059edec90465d9d1f544f94191a6
Daniel Arbuckle <djarb@highenergymagic.org>
2021-03-26 11:59:52 -0700

    basic k8s config, automation

--- Makefile
+++ Makefile
@@ -1,7 +1,16 @@
+RELEASE=0.1
+PATCH:=$(shell cat PATCH)
+
 frontend: frontend/Dockerfile
-	docker build -t circuit-frontend frontend
+	docker build -t "scistarter/circuit-frontend:$(RELEASE).$(PATCH)" frontend
 
 backend: backend/Dockerfile
-	docker build -t circuit-backend backend
+	docker build -t "scistarter/circuit-backend:$(RELEASE).$(PATCH)" backend
+
+upload: frontend backend
+	docker push "scistarter/circuit-frontend:$(RELEASE).$(PATCH)"
+	docker push "scistarter/circuit-backend:$(RELEASE).$(PATCH)"
+	echo "Uploaded $(RELEASE).$(PATCH)"
+	echo $$(( $(PATCH) + 1 )) > PATCH
 
 .PHONY: frontend backend
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,7 +8,8 @@ use tide::prelude::*;
 use tide_fluent_routes::prelude::*;
 use tide_websockets::{Message, WebSocket};
 
-async fn dummy(mut _req: tide::Request<()>) -> tide::Result {
+async fn dummy(mut req: tide::Request<()>) -> tide::Result {
+    println!("{:?}", req);
     Ok("Hello web world".into())
 }
 
@@ -17,8 +18,9 @@ async fn main() -> tide::Result<()> {
     let mut app = tide::new();
 
     // https://crates.io/crates/tide-fluent-routes
-    app.register(root().get(dummy));
+    app.register(root().get(dummy).at("api/", |routes| routes.get(dummy)));
 
+    println!("Listening on 0.0.0.0:8000");
     app.listen("0.0.0.0:8000").await?;
 
     Ok(())
