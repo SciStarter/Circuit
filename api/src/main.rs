7a835323116977dc38880f1da3c77fc13b8fd90c
Daniel Arbuckle <djarb@highenergymagic.org>
2021-03-31 14:21:11 -0700

    proper secret handling for api server, stub logger, database and migration support, tooling

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,21 +1,35 @@
 #![deny(unsafe_code)]
 
-use sqlx::postgres::Postgres;
+use sqlx::postgres::{PgPoolOptions, Postgres};
 use sqlx::prelude::*;
-use tide_sqlx::{SQLxMiddleware, SQLxRequestExt};
-
 use tide::prelude::*;
 use tide_fluent_routes::prelude::*;
+use tide_sqlx::{SQLxMiddleware, SQLxRequestExt};
 use tide_websockets::{Message, WebSocket};
 
+#[cfg(feature = "container")]
+const DATABASE_URL: &'static str = include_str!("/run/secrets/database_url");
+
+#[cfg(not(feature = "container"))]
+const DATABASE_URL: &'static str = env!("DATABASE_URL");
+
 async fn dummy(mut req: tide::Request<()>) -> tide::Result {
+    let mut db = req.sqlx_conn::<Postgres>().await;
     println!("{:?}", req);
     Ok("Hello web world".into())
 }
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
+    let pool = PgPoolOptions::new()
+        .min_connections(1)
+        .connect(DATABASE_URL)
+        .await?;
+
+    sqlx::migrate!("db/migrations").run(&pool).await?;
+
     let mut app = tide::new();
+    app.with(SQLxMiddleware::from(pool));
 
     // https://crates.io/crates/tide-fluent-routes
     app.register(root().get(dummy).at("api/", |routes| routes.get(dummy)));
