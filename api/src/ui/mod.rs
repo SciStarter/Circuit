45f5a60f066927c386f0c412c4970a94acdc4517
Daniel Arbuckle <djarb@highenergymagic.org>
2021-07-06 04:06:55 -0700

    more session support

--- api/src/ui/mod.rs
+++ api/src/ui/mod.rs
@@ -19,22 +19,41 @@ pub fn routes(routes: RouteSegment<()>) -> RouteSegment<()> {
         .at("external", |r| r.post(record_external))
         .at("login", |r| r.post(login))
         .at("signup", |r| r.post(signup))
+        .at("me", |r| r.get(me))
         .at("content", |r| r.get(content))
 }
 
+/// Update the clickstream with a single on-site click instance
 pub async fn record_click(mut _req: tide::Request<()>) -> tide::Result {
     Ok(Response::builder(StatusCode::Ok).build())
 }
 
+/// Record an instance of a user clicking on an external link
 pub async fn record_external(mut _req: tide::Request<()>) -> tide::Result {
     Ok(Response::builder(StatusCode::Ok).build())
 }
 
+/// Log the current session in to an account, if the validations pass.
+///
+/// Set a token cookie with the HttpOnly and (on
+/// #[cfg(not(debug_assertions))] ) Secure flags, and also return the
+/// token in the response body for script use.
 pub async fn login(mut _req: tide::Request<()>) -> tide::Result {
     unimplemented!()
 }
 
+/// Create an account, if the validations pass.
+///
+/// Set a token cookie with the HttpOnly and (on
+/// #[cfg(not(debug_assertions))] ) Secure flags, and also return the
+/// token in the response body for script use.
 pub async fn signup(mut _req: tide::Request<()>) -> tide::Result {
+    unimplemented!()
+}
+
+/// Retrieve UI-approriate information about the current user in JSON
+/// format. Includes the token, display name, and so on.
+pub async fn me(mut _req: tide::Request<()>) -> tide::Result {
     unimplemented!()
 }
 
