4b2b8ffe5a9bc88a4a6f5e4f1c544324f9cbfccd
Daniel Arbuckle <djarb@highenergymagic.org>
2021-07-28 12:18:23 -0700

    rip out tide-sqlx, set up ability to dynamically edit whole pages, create skeleton template for opportunity pages

---
+++ frontend/components/JustContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <dynamic-block class="content" :group="entity.slug" item="content"/>
+</template>
+
+<script>
+export default {
+  props: {
+    entity: {
+      type: Object,
+      required: true
+    }
+  },
+}
+</script>
---
+++ frontend/pages/_slug.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- Selects a top level component to display based on the data pulled from the server -->
+    <!-- If we add more top level components, they need to be added between just-content and opportunity, with a v-else-if="LOGIC" attribute -->
+    <just-content v-if="layout == 'just_content'" :entity="entity"/>
+    <opportunity v-else :entity="entity"/>
+  </div>
+</template>
+
+<script>
+import JustContent from '~/components/JustContent'
+import Opportunity from '~/components/Opportunity'
+
+export default {
+    name: 'Dynamic',
+
+    components: {
+        JustContent,
+        Opportunity,
+    },
+
+    async asyncData({ params, $axios }) {
+        const entity = await $axios.$get("/api/ui/entity/" + params.slug);
+        const layout = entity.entity_type.page ? entity.entity_type.page.layout : entity.entity_type;
+
+        return {entity, layout};
+    }
+}
+</script>
