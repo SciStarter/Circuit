751f453a2be87726fb5a88113e8f5438811bc9ec
Daniel Arbuckle <djarb@highenergymagic.org>
2021-09-03 08:24:18 -0700

    better psql launch script

--- k8s/psql.sh
+++ k8s/psql.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "Enter the Postgres password, even if you don't see a prompt"
-cat secrets/database.env|grep PASSWORD
-kubectl run -i --tty busybox --image=governmentpaas/psql -- sh -c 'psql -h postgres-beta -U postgres'
-kubectl delete pod busybox
+source secrets/database.env
+kubectl exec --stdin --tty `kubectl get pod -l app=postgres-beta -o name` -- /usr/bin/psql ${DATABASE_URL}
+
