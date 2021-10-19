4dbfdd288f4aa73f45079f597237fc6a87734270
Daniel Arbuckle <djarb@highenergymagic.org>
2021-10-19 12:16:07 -0700

    logs and clicks streams to linode object store

--- api/src/ui/activity.rs
+++ api/src/ui/activity.rs
@@ -1,9 +1,24 @@
 use common::Database;
+use once_cell::sync::Lazy;
 use tide::{Response, StatusCode};
 use tide_fluent_routes::{
     routebuilder::{RouteBuilder, RouteBuilderExt},
     RouteSegment,
 };
+
+static CLICK_ENDPOINT: Lazy<String> = Lazy::new(|| {
+    format!(
+        "http://{}:{}/internal/click",
+        std::env::var("CIRCUIT_LOGGER_SERVICE_SERVICE_HOST").unwrap_or_else(|_| std::env::var(
+            "CIRCUIT_LOGGER_SERVICE_BETA_SERVICE_HOST"
+        )
+        .unwrap_or_else(|_| "localhost".to_string())),
+        std::env::var("CIRCUIT_LOGGER_SERVICE_SERVICE_PORT").unwrap_or_else(|_| std::env::var(
+            "CIRCUIT_LOGGER_SERVICE_BETA_SERVICE_PORT"
+        )
+        .unwrap_or_else(|_| "9000".to_string())),
+    )
+});
 
 pub fn routes(routes: RouteSegment<Database>) -> RouteSegment<Database> {
     routes
@@ -13,12 +28,16 @@ pub fn routes(routes: RouteSegment<Database>) -> RouteSegment<Database> {
 
 /// Update the clickstream with a single on-site click instance. No-op
 /// when compiled in debug mode.
-pub async fn record_click(mut _req: tide::Request<Database>) -> tide::Result {
+pub async fn record_click(mut req: tide::Request<Database>) -> tide::Result {
     if cfg!(not(debug_assertions)) {
-        todo!()
-    } else {
-        Ok(Response::builder(StatusCode::Ok).build())
+        async_std::task::spawn(
+            surf::post(&*CLICK_ENDPOINT)
+                .body(req.body_json::<serde_json::Value>().await?)
+                .send(),
+        );
     }
+
+    Ok(Response::builder(StatusCode::Ok).build())
 }
 
 /// Record an instance of a user clicking on an external link. No-op
