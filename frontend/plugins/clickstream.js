fcf792ef6e67e065ff7403c9a5bda40ece23f16f
Daniel Arbuckle <djarb@highenergymagic.org>
2021-10-20 02:20:56 -0700

    fix security flaw in clickstream

--- frontend/plugins/clickstream.js
+++ frontend/plugins/clickstream.js
@@ -1,39 +1,40 @@
-document.body.addEventListener('click', async function (event) {
-    const params = {
-        session: window.localStorage.getItem('token') || '',
-        on_page: window.location.pathname + window.location.search + window.location.hash,
-        element_type: event.target.nodeName,
-        element_id: event.target.id,
-        element_class: event.target.className,
-        title: event.target.title
-    };
+document.body.addEventListener("click", async function (event) {
+  const params = {
+    session: window.localStorage.getItem("session") || "",
+    on_page: window.location.pathname + window.location.search +
+      window.location.hash,
+    element_type: event.target.nodeName,
+    element_id: event.target.id,
+    element_class: event.target.className,
+    title: event.target.title,
+  };
 
-    const context = event.target.closest('[data-context]');
-    if (context) {
-        params.context = context.dataset.context;
-    }
+  const context = event.target.closest("[data-context]");
+  if (context) {
+    params.context = context.dataset.context;
+  }
 
-    switch (event.target.nodeName) {
-    case 'A':
-        params.text = event.target.innerText.slice(0, 32).replace('\n', ' ');
-        params.href = event.target.href;
-        break;
-    case 'INPUT':
-    case 'BUTTON':
-        params.name = event.target.name;
-        params.value = event.target.value;
-        params.text = event.target.innerText.slice(0, 32).replace('\n', ' ');
-        break;
-    case 'DIV':
-    case 'SPAN':
-    case 'P':
-        params.text = event.target.innerText.slice(0, 32).replace('\n', ' ');
-        break;
-    case 'IMG':
-        params.text = event.target.alt;
-        break;
+  switch (event.target.nodeName) {
+    case "A":
+      params.text = event.target.innerText.slice(0, 32).replace("\n", " ");
+      params.href = event.target.href;
+      break;
+    case "INPUT":
+    case "BUTTON":
+      params.name = event.target.name;
+      params.value = event.target.value;
+      params.text = event.target.innerText.slice(0, 32).replace("\n", " ");
+      break;
+    case "DIV":
+    case "SPAN":
+    case "P":
+      params.text = event.target.innerText.slice(0, 32).replace("\n", " ");
+      break;
+    case "IMG":
+      params.text = event.target.alt;
+      break;
     default:
-    };
+  }
 
-    await window.$nuxt.$axios.$post('/api/ui/activity/click', params);
+  await window.$nuxt.$axios.$post("/api/ui/activity/click", params);
 });
--- frontend/plugins/refresh_user.js
+++ frontend/plugins/refresh_user.js
@@ -17,7 +17,24 @@ window.onNuxtReady(async () => {
   // Needed for either version
   if (user.authenticated) {
     window.localStorage.setItem("token", user.token);
+    if (window.isSecureContext) {
+      const hashed = btoa(
+        String.fromCharCode.apply(
+          null,
+          new Uint8Array(
+            await crypto.subtle.digest(
+              "SHA-256",
+              new ArrayBuffer(user.token + Math.random().toString().slice(2)),
+            ),
+          ),
+        ),
+      );
+      window.localStorage.setItem("session", hashed);
+    } else {
+      window.localStorage.setItem("session", "insecure");
+    }
   } else {
     window.localStorage.removeItem("token");
+    window.localStorage.removeItem("session");
   }
 });
