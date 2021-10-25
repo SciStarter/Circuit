4154c71358b569894be884abd24be325fa0bb5fa
Daniel Arbuckle <djarb@highenergymagic.org>
2021-10-25 03:18:09 -0700

    fixes issue #47

---
+++ frontend/components/LoadingSpinner.vue
@@ -0,0 +1,25 @@
+<template>
+<b-loading v-model="active" />
+</template>
+
+<script>
+export default {
+    name: "LoadingSpinner",
+
+    data() {
+        return {
+            active: false
+        };
+    },
+
+    mathods: {
+        start() {
+            this.active = true;
+        },
+
+        finish() {
+            this.active = false;
+        }
+    },
+}
+</script>
