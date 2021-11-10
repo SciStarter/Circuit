031e2be924f3e0296f8aeaff9b81502d8f7757fc
Daniel Arbuckle <djarb@highenergymagic.org>
2021-11-10 13:26:43 -0800

    fixes issue #121

--- frontend/components/OpportunityLocation.vue
+++ frontend/components/OpportunityLocation.vue
@@ -4,14 +4,34 @@
     <em>See map on opportunity page</em>
   </p>
   <p v-else-if="!isOpportunity && opportunity.location_point && opportunity.location_point.type === 'Point'">
-    <em>See map on opportunity page</em>
+    <li v-if="opportunity.location_name">
+      {{ opportunity.location_name }}
+    </li>
+    <li v-else-if="(opportunity.address_city || opportunity.address_state) && !short">
+      {{ opportunity.address_city }}, {{ opportunity.address_state }}
+    </li>
+    <em v-else>See map on opportunity page</em>
   </p>
   <p v-else-if="isOpportunity && opportunity.location_polygon && opportunity.location_polygon.type === 'MultiPolygon'">
     In a specific area
   </p>
-  <p v-else-if="isOpportunity && opportunity.location_point && opportunity.location_point.type === 'Point'">
-    In a specific area
-  </p>
+  <ul v-else-if="isOpportunity && opportunity.location_point && opportunity.location_point.type === 'Point'">
+    <li v-if="opportunity.location_name">
+      {{ opportunity.location_name }}
+    </li>
+    <li v-if="short">
+      {{ opportunity.address_city }}, {{ opportunity.address_state }}
+    </li>
+    <li v-if="opportunity.address_street && (!short || !opportunity.location_name)">
+      {{ opportunity.address_street }}
+    </li>
+    <li v-if="(opportunity.address_city || opportunity.address_state) && !short">
+      {{ opportunity.address_city }}, {{ opportunity.address_state }}
+    </li>
+    <li v-if="opportunity.address_country && !short">
+      {{ opportunity.address_country }}
+    </li>
+  </ul>
   <span v-else-if="opportunity.location_type == 'online'">Online</span>
   <span v-else-if="opportunity.location_type == 'any'">Anywhere</span>
   <ul v-else-if="opportunity.location_type == 'at'">
