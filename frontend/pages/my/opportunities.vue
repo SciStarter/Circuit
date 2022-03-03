9bb4ad5b97efe8c4b04a1ebdd66addfd7df2779f
Daniel Arbuckle <djarb@highenergymagic.org>
2022-03-03 09:06:26 -0800

    fix tab labels

--- frontend/pages/my/opportunities.vue
+++ frontend/pages/my/opportunities.vue
@@ -7,8 +7,8 @@
 
   <ul class="nav-tabs">
       <li><a class="tab-link":class="{'active':state==1}" @click="state=1">Current, Live Opportunities</a></li>
-      <li><a class="tab-link":class="{'active':state==2}" @click="state=2">Draft &amp; Unpublished</a></li>
-      <li><a class="tab-link":class="{'active':state==3}" @click="state=3">Expired &amp; Trashed</a></li>
+      <li><a class="tab-link":class="{'active':state==2}" @click="state=2">Draft, Unpublished &amp; Expired</a></li>
+      <li><a class="tab-link":class="{'active':state==3}" @click="state=3">Trashed</a></li>
       <li class="push-right"><action-button text2 @click="export_records">Export Records</action-button></li>
   </ul>
 
