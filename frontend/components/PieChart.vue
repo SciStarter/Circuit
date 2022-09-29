91c31c1048e90d4b8601e1dce7c78adcb077577b
Daniel Arbuckle <djarb@highenergymagic.org>
2022-09-29 08:27:42 -0700

    pie chart skeleton

---
+++ frontend/components/PieChart.vue
@@ -0,0 +1,21 @@
+<template>
+<div ref="display" />
+</template>
+
+<script>
+import * as d3 from 'd3'
+
+export default {
+    name: "PieChart",
+
+    props: {
+
+    },
+
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
