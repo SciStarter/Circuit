acc9b926ed733e0832b19197d3f91ce9d3ec0e8e
Daniel Arbuckle <djarb@highenergymagic.org>
2023-05-31 04:31:25 -0700

    fixes #243

--- frontend/components/PartnerForm.vue
+++ frontend/components/PartnerForm.vue
@@ -60,7 +60,7 @@
 
 
   <div class="global-actions">
-    <a href="https://sciencenearme.org/my/organization" target="_blank">Edit on Science Near Me</a>
+    <a v-if="inExchange" href="https://sciencenearme.org/my/organization" target="_blank">Edit on Science Near Me</a>
   </div>
 
   <b-modal
