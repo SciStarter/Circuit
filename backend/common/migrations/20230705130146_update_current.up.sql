a8fccf7d6d46ba199bf369a59c3a2e4533401dbe
Daniel Arbuckle <djarb@highenergymagic.org>
2023-07-05 06:33:04 -0700

    tweak opportunity current definition

--- backend/common/migrations/20230705130146_update_current.up.sql
+++ backend/common/migrations/20230705130146_update_current.up.sql
@@ -9,29 +9,15 @@ BEGIN
    interior ->> 'withdrawn' = 'false'
    AND
    (
-     (
-       EXISTS (SELECT value FROM jsonb_array_elements_text(exterior -> 'start_datetimes') WHERE value::timestamptz < stamp)
-       AND
        (
-         EXISTS (SELECT value FROM jsonb_array_elements_text(exterior -> 'end_datetimes') WHERE value::timestamptz > stamp)
-         OR
-         (
-           jsonb_array_length(exterior -> 'end_datetimes') = 0
-           AND
-           EXISTS (SELECT value FROM jsonb_array_elements_text(exterior -> 'start_datetimes') WHERE value::timestamptz + interval '1 hour' > stamp)
-         )
+          jsonb_array_length(exterior -> 'start_datetimes') = 1
+          AND
+          jsonb_array_length(exterior -> 'end_datetimes') = 0
        )
-     )
-     OR
-     EXISTS (SELECT value FROM jsonb_array_elements_text(exterior -> 'start_datetimes') WHERE value::timestamptz > stamp)
-     OR
-     EXISTS (SELECT value FROM jsonb_array_elements_text(exterior -> 'end_datetimes') WHERE value::timestamptz > stamp)
-     OR
-     (
-       jsonb_array_length(exterior -> 'start_datetimes') = 0
-       AND
-       jsonb_array_length(exterior -> 'end_datetimes') = 0
-     )
+       OR
+       EXISTS (SELECT value FROM jsonb_array_elements_text(exterior -> 'start_datetimes') WHERE value::timestamptz > stamp)
+       OR
+       EXISTS (SELECT value FROM jsonb_array_elements_text(exterior -> 'end_datetimes') WHERE value::timestamptz > stamp)
    )
  );
 END
